b5ec769f84b2a065544ee319b1a4d206f7122ba1
Kimutaijeremy <jrmkimutai@gmail.com>
2025-11-27 02:45:55 +0300

    sequences lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+        return
+    if length == 1:
+        print([0])
+        return
+    if length == 2:
+        print([0, 1])
+        return
+
+    fib = [0, 1]
+    while len(fib) < length:
+        next_value = fib[-1] + fib[-2]
+        fib.append(next_value)
+
+    print(fib)
